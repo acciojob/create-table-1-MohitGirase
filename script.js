6c2ba8e201ad21025f49f6df202f97bdbf2b7650
Mohit Girase <114944481+MohitGirase@users.noreply.github.com>
2025-11-22 05:27:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+
 function insert_Row() {
     //Write your code here
-  
-  
+	
+	const cell1 = document.createElement("td");
+	cell1.textContent = `New Cell1`;
+	
+	const cell2 = document.createElement("td");
+	cell2.textContent = `New Cell2`;
+	
+	const newRow = document.createElement("tr");
+	newRow.appendChild(cell1);
+	newRow.appendChild(cell2);
+
+	const table = document.getElementById("sampleTable");
+	table.prepend(newRow);
 }
+
