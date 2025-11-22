f76605201fa560f6bb9b8b06f9200f63c5400f1b
Mohit Girase <114944481+MohitGirase@users.noreply.github.com>
2025-11-22 05:35:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 
 function insert_Row() {
     //Write your code here
-	
-	const cell1 = document.createElement("td");
-	cell1.textContent = "New Cell1";
-	
-	const cell2 = document.createElement("td");
-	cell2.textContent = "New Cell2";
-	
-	const newRow = document.createElement("tr");
-	newRow.appendChild(cell1);
-	newRow.appendChild(cell2);
-
 	const table = document.getElementById("sampleTable");
-	table.prepend(newRow);
+	
+    const newRow = table.insertRow(0);
+
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
 }
 
